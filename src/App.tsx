a5990a201951beb95abde3f47b3e3fb5105212d5
Kento Chiba <c.kento.chiba@gmail.com>
2019-07-08 23:38:56 +0900

    Add test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export const sampleFunc = (value:number) => {
+  return value + 1
+}
+
 const App: React.FC = () => {
   return (
     <div className="App">
